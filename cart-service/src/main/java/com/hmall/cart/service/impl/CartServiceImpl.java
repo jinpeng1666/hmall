bc75c78c34e877a90b52475ba16ed5ff7420c850
JP.Zhou <3214423826@qq.com>
2025-03-14 14:50:14 +0800

    refactor: 优化查询购物车功能（服务注册与发现、ip地址硬编码、负载均衡）

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +17,8 @@ import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +48,8 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 //    private final IItemService itemService;
 
     private final RestTemplate restTemplate;
+
+    private final DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -95,9 +100,18 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 2.查询商品
         // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
 
-        // 2.1.利用RestTemplate发起http请求，得到http的响应
+        // 2.1 根据服务名称获取服务的实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        if (CollUtil.isEmpty(instances)) {
+            return;
+        }
+
+        // 2.2 模拟负载均衡，从实例列表中挑选一个实例
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+
+        // 2.3.利用RestTemplate发起http请求，得到http的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
@@ -105,7 +119,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
                 Map.of("ids", CollUtil.join(itemIds, ","))
         );
 
-        // 2.2.解析响应
+        // 2.4.解析响应
         if(!response.getStatusCode().is2xxSuccessful()){
             // 查询失败，直接结束
             return;
